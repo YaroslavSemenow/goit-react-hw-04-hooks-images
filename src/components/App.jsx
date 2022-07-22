30fad2cf92dc4e1f384c2b60da6b304710fb9d14
Yaroslav Semenov <yaryk2013@gmail.com>
2022-07-22 21:24:34 +0300

    app in hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import style from './App.module.css';
 import { getPhotos } from 'service/Api-service';
 import Searchbar from './Searchbar';
@@ -7,32 +7,26 @@ import ImageGallery from './ImageGallery';
 import Modal from './Modal';
 import Button from './Button';
 
-class App extends Component {
-  state = {
-    photos: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    showLoadMoreBtn: false,
-    showModal: false,
-    modalImg: {
-      src: '',
-      alt: '',
-    },
-  };
+export default function App() {
+  const [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImg, setModalImg] = useState({
+    src: '',
+    alt: '',
+  });
 
-  componentDidUpdate = async (_, prevState) => {
-    if (
-      this.state.query !== prevState.query ||
-      this.state.page !== prevState.page
-    ) {
-      this.setState({
-        isLoading: true,
-        showLoadMoreBtn: false,
-      });
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
 
-      const { photos, query, page } = this.state;
+    setIsLoading(true);
 
+    const fetchPhotos = async () => {
       try {
         const data = await getPhotos(query, page);
         const totalPhotos = data.totalHits;
@@ -50,70 +44,58 @@ class App extends Component {
           return;
         }
 
-        this.setState({
-          photos: [...photos, ...newPhotos],
-          showLoadMoreBtn: newPhotos.length === 12 && totalPhotos > 12,
-        });
+        setPhotos(photos => [...photos, ...newPhotos]);
+        setShowLoadMoreBtn(newPhotos.length === 12 && totalPhotos > 12);
       } catch (error) {
         console.log(error);
         alert('Oops, something went wrong. Please, reload the page');
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
+    };
+
+    fetchPhotos();
+  }, [page, query]);
+
+  const handlerSubmitForm = async query => {
+    setPhotos([]);
+    setQuery(query);
+    setPage(1);
+    setShowLoadMoreBtn(false);
   };
 
-  handlerSubmitForm = async query => {
-    this.setState({
-      photos: [],
-      query,
-      page: 1,
+  const getMorePhotos = async () => {
+    setPage(page => page + 1);
+  };
+
+  const getModalImg = (src, alt) => {
+    setModalImg({
+      src,
+      alt,
     });
   };
 
-  getMorePhotos = async () => {
-    this.setState(({ page }) => ({
-      page: page + 1,
-    }));
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
 
-  getModalImg = (src, alt) => {
-    this.setState({
-      modalImg: {
-        src,
-        alt,
-      },
-    });
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  render() {
-    const { photos, isLoading, showLoadMoreBtn, showModal, modalImg } =
-      this.state;
-
-    return (
-      <div className={style.app}>
-        <Searchbar onSubmit={this.handlerSubmitForm} />
-        {photos.length > 0 && (
-          <ImageGallery
-            photos={photos}
-            getModalImg={this.getModalImg}
-            openModal={this.toggleModal}
-          />
-        )}
-        {isLoading && <Loader />}
-        {showLoadMoreBtn && <Button onLoadMore={this.getMorePhotos} />}
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={modalImg.src} alt={modalImg.alt} />
-          </Modal>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className={style.app}>
+      <Searchbar onSubmit={handlerSubmitForm} />
+      {photos.length > 0 && (
+        <ImageGallery
+          photos={photos}
+          getModalImg={getModalImg}
+          openModal={toggleModal}
+        />
+      )}
+      {isLoading && <Loader />}
+      {showLoadMoreBtn && <Button onLoadMore={getMorePhotos} />}
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={modalImg.src} alt={modalImg.alt} />
+        </Modal>
+      )}
+    </div>
+  );
 }
-
-export default App;
